2b86a7fd3a22454e1b131a5f463f18a297ec736b
Benjamin Rosas <Benjamin Rosas>
2019-07-08 23:44:59 -0500

    ok I guess I dont need this

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,3 @@
 import Route from '@ember/routing/route'
-import { action } from '@ember/object'
 
-export default class HomeRoute extends Route {
-  @action
-  didTransition() {
-    this._super(...arguments)
-    return true
-  }
-}
+export default class HomeRoute extends Route {}
